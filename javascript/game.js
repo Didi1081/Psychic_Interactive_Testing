d5efe0018361df1ecbdbfc731467d8e15092c8ed
Didi <ddapp108@gmail.com>
2018-12-05 08:07:14 -0600

    refactor game.js

--- javascript/game.js
+++ javascript/game.js
@@ -1,14 +1,14 @@
 
     // These are the choices that the player has to guess. 
     //  <link rel="stylesheet" type="text/css" media="screen" href="main.css"/>
-    var computerGuesses =  ["d","i","d","i"];
+    const computerGuesses =  ["d","i","d","i"];
 
 
     // This is where the wins, losses and scoring are
-    var wins = 0;
-    var losses = 0;
-    var guessesLeft = 10;
-    var wrongGuess= [];
+    let wins = 0;
+    let losses = 0;
+    let guessesLeft = 10;
+    let wrongGuess= [];
 
 // This will be the reset of the game, it makes it start to 10 again, but doesn't clear out the 
     
@@ -21,21 +21,21 @@
     
 
     // This is where the references will be.
-    var directionsHere = document.getElementById("directions");
-    var userLettersHere = document.getElementById("userChooses");
-    var computerChoiceHere = document.getElementById("computerChooses");
-    var winsText = document.getElementById("winspara");
-    var lossesText = document.getElementById("lossespara");
-    var guessesText = document.getElementById("numberOfGuessesLeft");
-    var computerGuess;
-    var userGuess;
-    //var guessesLeftText = document.getElementByID("numberOfGuessesLeft" + guessesLeft);
+    let directionsHere = document.getElementById("directions");
+    let userLettersHere = document.getElementById("userChooses");
+    let computerChoiceHere = document.getElementById("computerChooses");
+    let winsText = document.getElementById("winspara");
+    let lossesText = document.getElementById("lossespara");
+    let guessesText = document.getElementById("numberOfGuessesLeft");
+    let computerGuess;
+    let userGuess;
+    //let guessesLeftText = document.getElementByID("numberOfGuessesLeft" + guessesLeft);
    
     
     //The user needs to press a key and it needs to be detected
     document.onkeyup = function (event) { if( event.keyCode >= 65 && event.keyCode <= 90){
 
-      var userGuess = event.key;
+      let userGuess = event.key;
       computerGuess = computerGuesses[Math.floor(Math.random() * computerGuesses.length)];
 
       
